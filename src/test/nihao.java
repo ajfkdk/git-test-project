86b86bd949889b1e85a77c6eabf53f09b6a64710
adminzhangsan <2598301006@qq.com>
2021-01-29 09:47:55 +0800

    把项目初始化（测试）

---
+++ src/test/nihao.java
@@ -0,0 +1,14 @@
+package test;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class nihao {
+    public static void main(String[] args) {
+        System.out.println("你好git");
+        System.out.println("你好git");
+        System.out.println("你好git");
+        System.out.println("你好git");
+        System.out.println("你好git");
+        System.out.println("你好git");
+    }
+}
